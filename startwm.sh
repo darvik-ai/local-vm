0e3586fa019ba78c5244a9650430034a6f0c75c5
darvik-ai <159222738+darvik-ai@users.noreply.github.com>
2025-09-21 06:44:03 -0700

    Update startwm.sh

--- startwm.sh
+++ startwm.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
-#
-# This script is executed by xrdp to start the user's desktop environment.
-# This is configured to start an XFCE4 session.
 
-# Unset variables that might interfere with the session
+# Fix for grey screen issue on XFCE
+unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
-unset XDG_RUNTIME_DIR
-
-# Load system-wide environment variables
-. /etc/profile
 
 # Start the XFCE4 desktop environment
-startxfce4
+startxfce4 &
