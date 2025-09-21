7b3c615d34fc0b972234244ee617a0f18087baf5
darvik-ai <159222738+darvik-ai@users.noreply.github.com>
2025-09-21 06:52:21 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ sudo /usr/local/sbin/guacd -b 0.0.0.0 -l 4822 &
 
 # Start xrdp services as root in the background
 echo "Starting xrdp..."
-sudo service xrdp start &
+sudo service vncserver start &
 
 # Wait until guacd is listening on its port
 echo "Waiting for guacd to be ready..."
